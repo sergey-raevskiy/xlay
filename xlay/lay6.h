bd1711e4dd1c8495d4d93a02ebff7b179ac6d404
sergey-raevskiy <ccik@inbox.ru>
2019-11-27 16:08:13 +0300

    Something.

--- xlay/lay6.h
+++ xlay/lay6.h
@@ -29,12 +29,47 @@ struct LAY_FileHeader
 
 ASSERT_SIZE(LAY_FileHeader, 8);
 
+enum
+{
+    LAY_C1,
+    LAY_S1,
+    LAY_C2,
+    LAY_S2,
+    LAY_I1,
+    LAY_I2,
+    LAY_O,
+};
+
 #pragma pack(push, 1)
 struct LAY_BoardHeader
 {
     LAY_String<30> name;
-    UCHAR pad[534 - 31];
+
+    UCHAR __pad0[4];
+    DWORD size_x;
+    DWORD size_y;
+
+    UCHAR ground_pane[7];
+    UCHAR __pad1[24];
+
+    UCHAR active_layer;
+    UCHAR __pad2[3]; // DWORD?
+
+    UCHAR layer_visible[7];
+    UCHAR show_scanned_copy_top;
+    UCHAR show_scanned_copy_bottom;
+
+    LAY_String<200> scanned_copy_top_path;
+    LAY_String<200> scanned_copy_bottom_path;
+
+    UCHAR __pad3[45];
 };
 #pragma pack(pop)
 
 ASSERT_SIZE(LAY_BoardHeader, 534);
+ASSERT_FIELD_OFFSET(LAY_BoardHeader, size_x, 0x23);
+ASSERT_FIELD_OFFSET(LAY_BoardHeader, size_y, 0x27);
+ASSERT_FIELD_OFFSET(LAY_BoardHeader, ground_pane, 0x2b);
+ASSERT_FIELD_OFFSET(LAY_BoardHeader, layer_visible, 0x4e);
+ASSERT_FIELD_OFFSET(LAY_BoardHeader, scanned_copy_top_path, 0x57);
+ASSERT_FIELD_OFFSET(LAY_BoardHeader, scanned_copy_bottom_path, 0x120);
--- xlay/main.cpp
+++ xlay/main.cpp
@@ -4,5 +4,7 @@
 int main()
 {
     printf("%d\n", sizeof(LAY_BoardHeader));
+    printf("%x\n", FIELD_OFFSET(LAY_BoardHeader, size_x));
+    printf("%x\n", FIELD_OFFSET(LAY_BoardHeader, layer_visible));
     system("pause");
 }
